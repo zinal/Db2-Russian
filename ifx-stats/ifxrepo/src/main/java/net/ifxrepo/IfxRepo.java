ef49be74e7588c0d9eae530dc369cefad969907c
Maksim Zinal <mzinal@ru.ibm.com>
2022-04-29 10:04:25 +0300

    *

--- ifx-stats/ifxrepo/src/main/java/net/ifxrepo/IfxRepo.java
+++ ifx-stats/ifxrepo/src/main/java/net/ifxrepo/IfxRepo.java
@@ -1,13 +1,36 @@
 package net.ifxrepo;
 
+import java.io.File;
+
 /**
  *
  * @author zinal
  */
-public class IfxRepo {
+public class IfxRepo implements Runnable {
+
+    private final Settings settings;
+
+    public IfxRepo(Settings settings) {
+        this.settings = settings;
+    }
 
     public static void main(String[] args) {
-        
+        String jobFile = "ifxrepo-job.xml";
+        if (args.length > 0) {
+            jobFile = args[0];
+        }
+        try {
+            new IfxRepo(new Settings(new File(jobFile))) . run();
+        } catch(Exception ex) {
+            System.err.println("FATAL: unexpected error " + ex);
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ifx-repo 1.0 " + settings.getMode());
     }
 
 }
