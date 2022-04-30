26f752f5b6bbc18b1f2e15f40abc7f3b509c9354
Maksim Zinal <mzinal@ru.ibm.com>
2022-04-30 17:08:12 +0300

    нормализация запросов по значениям параметров

--- ifx-stats/ifxrepo/src/main/java/net/ifxrepo/RecQuery.java
+++ ifx-stats/ifxrepo/src/main/java/net/ifxrepo/RecQuery.java
@@ -1,9 +1,111 @@
 package net.ifxrepo;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+
 /**
  *
  * @author zinal
  */
 public class RecQuery {
+
+    private final String key;        // hash of normalized SQL
+    private final String sqlNorm;    // normalized SQL
+    private final String sqlSample;  // original SQL example
+
+    public RecQuery(String sql) {
+        this.sqlSample = sql;
+        this.sqlNorm = normalize(sql);
+        this.key = hash(this.sqlNorm);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getSqlNorm() {
+        return sqlNorm;
+    }
+
+    public String getSqlSample() {
+        return sqlSample;
+    }
+
+    public static String normalize(String sql) {
+        final StringBuilder sb = new StringBuilder();
+        for (int ix = 0; ix < sql.length(); ++ix) {
+            final char c1 = sql.charAt(ix);
+            switch (c1) {
+                case '\n': case '\r': case '\t': case ' ':
+                    sb.append(' ');
+                    break;
+                case '\'':
+                    sb.append("\'?\'");
+                    while (ix+1 < sql.length()) {
+                        final char c2 = sql.charAt(ix+1);
+                        if (c2=='\'') {
+                            if (ix+2 < sql.length()) {
+                                final char c3 = sql.charAt(ix+2);
+                                if (c3=='\'')
+                                    ++ix;
+                                else
+                                    break;
+                            }
+                        }
+                        ++ix;
+                    }
+                    break;
+                case '\"':
+                    sb.append(c1);
+                    while (ix+1 < sql.length()) {
+                        final char c2 = sql.charAt(ix+1);
+                        sb.append(c2);
+                        if (c2=='\"') {
+                            if (ix+2 < sql.length()) {
+                                final char c3 = sql.charAt(ix+2);
+                                if (c3=='\"') {
+                                    sb.append(c3);
+                                    ++ix;
+                                } else {
+                                    ++ix;
+                                    break;
+                                }
+                            }
+                        }
+                        ++ix;
+                    }
+                    break;
+                default:
+                    if (isNumeric(c1)) {
+                        sb.append('?');
+                        while (ix+1 < sql.length()) {
+                            final char c2 = sql.charAt(ix+1);
+                            if (! (isNumeric(c2) || (c2=='.')) ) {
+                                break;
+                            }
+                            ++ix;
+                        }
+                    } else {
+                            sb.append(c1);
+                    }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static boolean isNumeric(char c) {
+        return (c>='0') && (c<='9');
+    }
+
+    public static String hash(String v) {
+        try {
+            return Base64.getEncoder()
+                    .encodeToString(MessageDigest.getInstance("SHA-256")
+                            .digest(v.getBytes(StandardCharsets.UTF_8)));
+        } catch(Exception ex) {
+            throw new RuntimeException("hash(sql) failed", ex);
+        }
+    }
 
 }
---
+++ ifx-stats/ifxrepo/src/test/java/net/ifxrepo/RecQueryTest.java
@@ -0,0 +1,30 @@
+package net.ifxrepo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author zinal
+ */
+public class RecQueryTest {
+
+    @Test
+    public void normalizeTest() {
+        String in, out;
+
+        in = "SELECT x.a, y.b, z.c FROM x, y, z WHERE x.a=y.a AND x.b=y.b AND y.d=60\n "
+                + "AND z.f='Bamboleyo!'";
+        out = RecQuery.normalize(in);
+        Assert.assertEquals("SELECT x.a, y.b, z.c FROM x, y, z "
+                + "WHERE x.a=y.a AND x.b=y.b AND y.d=?  AND z.f='?'", out);
+
+        in = "SELECT x.a, y.b, z.c FROM x, y, \"ah'govno\" z "
+                + "WHERE x.a=y.a AND x.b=y.b AND y.d=60\n "
+                + "AND z.f='Bam''boleyo!'''";
+        out = RecQuery.normalize(in);
+        Assert.assertEquals("SELECT x.a, y.b, z.c FROM x, y, \"ah'govno\" z "
+                + "WHERE x.a=y.a AND x.b=y.b AND y.d=?  AND z.f='?'", out);
+    }
+
+}
